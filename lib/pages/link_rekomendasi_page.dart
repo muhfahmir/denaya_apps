0d4eccd5bdfb2c79007741a270440af910ccfac9
@faisal.achmad <faisal.achmad@kopnus.com>
2023-10-01 21:52:48 +0700

    sudah jadi

--- lib/pages/link_rekomendasi_page.dart
+++ lib/pages/link_rekomendasi_page.dart
@@ -45,6 +45,36 @@ class _LinkRekomendasiPageState extends State<LinkRekomendasiPage> {
       'name': 'Dart',
       'link': 'https://dart.dev/',
       'image': 'https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/dart-programming-language-icon.png',
+    },
+    {
+      'id': 6,
+      'name': 'Golang',
+      'link': 'https://go.dev/',
+      'image': 'https://go.dev/blog/go-brand/Go-Logo/PNG/Go-Logo_Blue.png',
+    },
+    {
+      'id': 7,
+      'name': 'Vue',
+      'link': 'https://vuejs.org/',
+      'image': 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/1200px-Vue.js_Logo_2.svg.png',
+    },
+    {
+      'id': 8,
+      'name': 'React',
+      'link': 'https://react.dev/',
+      'image': 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png',
+    },
+    {
+      'id': 9,
+      'name': 'Javascript',
+      'link': 'https://developer.mozilla.org/en-US/docs/Web/JavaScript',
+      'image': 'https://academy.alterra.id/blog/wp-content/uploads/2021/07/Logo-Javascript.png',
+    },
+    {
+      'id': 10,
+      'name': 'Python',
+      'link': 'https://www.python.org/',
+      'image': 'https://assets.stickpng.com/images/5848152fcef1014c0b5e4967.png',
     }
   ];
 
